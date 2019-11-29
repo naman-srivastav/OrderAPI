9ccde4b828301635c918b6287d19c0c453c6e2c2
Naman Srivastav <naman.srivastav@nagarro.com>
2019-11-29 12:08:54 +0530

    changed datatype of latitude and longitude

--- task/database/migrations/2019_11_13_063323_create_order_table.php
+++ task/database/migrations/2019_11_13_063323_create_order_table.php
@@ -15,10 +15,10 @@ class CreateOrderTable extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->float('origin_lat', 10, 6);
-            $table->float('origin_long', 10, 6);
-            $table->float('destination_lat', 10, 6);
-            $table->float('destination_long', 10, 6);
+            $table->string('origin_lat');
+            $table->string('origin_long');
+            $table->string('destination_lat');
+            $table->string('destination_long');
             $table->double('distance', 10, 2);
             $table->string('status', 10);
             $table->timestamps();
